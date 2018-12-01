4cf6c68e2b1c8fd7f88e5991fa10ad2c5668f5eb
WhatsCS <whatscs@jointheb.org>
2018-11-30 16:10:01 -0800

    Added basic object specific permissions

--- MysticalIndexer/api/models.py
+++ MysticalIndexer/api/models.py
@@ -4,6 +4,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.utils import timezone
 from rest_framework.authtoken.models import Token
+from dry_rest_permissions.generics import authenticated_users, allow_staff_or_superuser
 
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
@@ -12,13 +13,38 @@ def create_auth_token(sender, instance=None, created=False, **kwargs):
         Token.objects.create(user=instance)
 
 
+def user_upload_path(instance, filename):
+    return f'user_{instance.owner.id}/{filename}'
+
+
 class Upload(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=120)
     filename = models.CharField(max_length=60)
+    file = models.FileField(upload_to=user_upload_path)
     created = models.DateTimeField()
     type = models.CharField(max_length=32)
     owner = models.ForeignKey('auth.User', related_name='uploads', on_delete=models.CASCADE)
+
+    # Allow general read permissions
+    @staticmethod
+    def has_read_permission(request):
+        return True
+
+    # Allow specific objects to be read
+    def has_object_read_permission(self, request):
+        return True
+
+    # Allow all write access for people that are authed
+    @staticmethod
+    @authenticated_users
+    def has_write_permission(request):
+        return True
+
+    # Only allow owner or admins to edit/delete
+    @allow_staff_or_superuser
+    def has_object_write_permission(self, request):
+        return request.user == self.owner
 
     def save(self, *args, **kwargs):
         ''' Overload save and update created timestamp and add user '''
