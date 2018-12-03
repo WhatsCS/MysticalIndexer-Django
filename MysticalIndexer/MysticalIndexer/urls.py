e51dfc438a75b665d30e8f0cb2b8fb89cacc8be0
WhatsCS <whatscs@jointheb.org>
2018-12-03 14:59:23 -0800

    setting up some urls

--- MysticalIndexer/MysticalIndexer/urls.py
+++ MysticalIndexer/MysticalIndexer/urls.py
@@ -13,10 +13,30 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework.permissions import AllowAny
+
+# Swagger UI generation setup
+schema_view = get_schema_view(
+    openapi.Info(
+        title="MysticalIndexer API",
+        default_version="v1",
+        description="????",
+    ),
+    validators=['ssv'],
+    public=True,
+    permission_classes=(AllowAny,),
+)
 
 urlpatterns = [
-    path('v1/', include('api.urls')),
+    path('v1/', include(('api.urls', 'api'), namespace='v1')),
     path('admin/', admin.site.urls),
-]
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json-yaml'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT, show_indexes=True)
