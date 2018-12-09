757eb20cc1cadbd082cfb93d318c85fd38fe5097
WhatsCS <whatscs@jointheb.org>
2018-12-09 14:29:56 -0800

    update storage system, add emoji support :100:

--- MysticalIndexer/api/serializers.py
+++ MysticalIndexer/api/serializers.py
@@ -1,23 +1,29 @@
 from .models import Upload
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from hashid_field.rest import HashidSerializerCharField
 
 
-class UserSerializer(serializers.ModelSerializer):
-    uploads = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    uploads = serializers.PrimaryKeyRelatedField(
+        pk_field=HashidSerializerCharField(source_field='api.Upload.id'),
+        many=True, read_only=True
+    )
 
     class Meta:
         model = User
         fields = ('id', 'username', 'uploads',)
 
 
-class UploadSerializer(serializers.HyperlinkedModelSerializer):
+class UploadSerializer(serializers.ModelSerializer):
+    id = HashidSerializerCharField(source_field='api.Upload.id', read_only=True)
     owner = serializers.ReadOnlyField(source='owner.username')
     created = serializers.ReadOnlyField()
     filename = serializers.ReadOnlyField()
-    file = serializers.FileField(allow_empty_file=False, use_url=False, write_only=True)
-# TODO: Explore options for serving an aliased direct link to the image.
+    file = serializers.FileField(allow_empty_file=False, use_url=False)
+    url = serializers.ReadOnlyField()
+    # url = serializers.HyperlinkedIdentityField(view_name='v1:upload-detail')
 
     class Meta:
         model = Upload
-        fields = ('url', 'id', 'title', 'owner', 'created', 'file', 'filename')
+        fields = ('id', 'title', 'owner', 'created', 'file', 'filename', 'url',)
--- MysticalIndexer/api/views.py
+++ MysticalIndexer/api/views.py
@@ -5,6 +5,8 @@ from .utils.hashing import random_emojis, blake2b_hashing
 from django.conf import settings
 from django.contrib.auth.models import User
 from rest_framework import generics, mixins, permissions, parsers, viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from dry_rest_permissions.generics import DRYPermissions
 
 
@@ -19,13 +21,22 @@ class UserDetail(generics.RetrieveAPIView):
     serializer_class = UserSerializer
     permission_classes = (permissions.IsAuthenticated,)
 
+    @action(detail=False)
+    def show_uploads(self, request):
+        uploads = Upload.objects.filter(owner=request.query_params.id)
+        page = self.paginate_queryset(uploads)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(uploads, many=True)
+        return Response(serializer.data)
+
 
 class UploadViewSet(viewsets.ModelViewSet):
     queryset = Upload.objects.all()
     serializer_class = UploadSerializer
     permission_classes = (DRYPermissions,)
-    parser_classes = (parsers.FileUploadParser, parsers.FormParser,
-                      parsers.MultiPartParser,)
 
     def perform_create(self, serializer):
         # file object for manipulation
@@ -33,11 +44,11 @@ class UploadViewSet(viewsets.ModelViewSet):
         name, ext = file_object.name.split('.')
         #filename = random_emojis()
         #filename += '.' + ext
-        name = blake2b_hashing(name)
+        name = random_emojis()
         file_object.name = name + '.' + ext
 
         # save both the user as owner and the newly edited file
-        serializer.save(owner=self.request.user, file=file_object, filename=file_object.name)
+        serializer.save(owner=self.request.user, file=file_object, url='{0}{1}'.format(settings.MEDIA_URL, file_object.name))
 
     def perform_destroy(self, instance):
         os.remove(os.path.join(settings.MEDIA_ROOT, instance.file.path))
